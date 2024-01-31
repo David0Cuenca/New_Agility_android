b0f31e26c68769c0e623385926ea5ded151d053f
David0Cuenca <davcuequi@alu.edu.gva.es>
2024-01-31 12:18:29 +0100

    Dashboard ahora es scrolleable pero no tiene espacios entre cards

--- app/src/main/java/com/example/proyect_newagility/Dashboard.kt
+++ app/src/main/java/com/example/proyect_newagility/Dashboard.kt
@@ -1,35 +1,33 @@
 package com.example.proyect_newagility
 
-import android.provider.CalendarContract.Colors
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.List
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
@@ -54,16 +52,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.proyect_newagility.ui.theme.Blue
 import com.example.proyect_newagility.ui.theme.Primary
-import com.example.proyect_newagility.ui.theme.Typography
 import kotlinx.coroutines.launch
 import model.NavigationItem
 import model.Screens
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 
 @Composable
@@ -96,16 +93,10 @@ fun ScaffoldMain(navigationController: NavHostController){
             topBar = {
                 TopBarNav() {scope.launch { drawerState.open() }}},
             content = {innerPadding ->
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding),
-                    contentAlignment = Alignment.Center){
-                    BodyDashboard()
-                }
-
+                BodyDashboard(innerPadding)
             },
             floatingActionButton = {
-                Footer (
+                FooterDashboard (
                     navigationController = navigationController
                 ) },
             floatingActionButtonPosition = FabPosition.End
@@ -180,121 +171,66 @@ fun NavigationDrawerDashboard(navigationController: NavHostController, onCloseDr
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun BodyDashboard(){
-Column {
-    Card(modifier = Modifier
-        .fillMaxWidth(),
-        colors = CardDefaults.cardColors(
-            containerColor = Blue
-        )
-    ) {
-        Column(modifier = Modifier.padding(15.dp)) {
-            Row {
-                Icon(
-                    imageVector = Icons.Default.DateRange,
-                    contentDescription = "Date",
-                    tint = Color.Black
-                )
-                Spacer()
-                Text(
-                    text = "Reporte diario",
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Black
-                )
-            }
-            Spacer()
-            Row(modifier = Modifier) {
-                Icon(
-                    imageVector = Icons.Default.ShoppingCart,
-                    contentDescription = null, Modifier.size(150.dp),
-                    tint = Color.Black
-                )
-                Column(modifier = Modifier.align(Alignment.CenterVertically)) {
-                    Text(text = "Hay un total de trabajos: ", color = Color.Black)
-                    Text(text = getProyectDetails().size.toString(), color = Color.Black)
-                }
-            }
-        }
+fun BodyDashboard(innerPaddingValues: PaddingValues){
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState())
+        .padding(innerPaddingValues),
+    verticalArrangement = Arrangement.SpaceAround) {
+        CardTotal()
+        CardTotal()
+        CardTotal()
+        CardTotal()
+        CardTotal()
     }
-    Card(modifier = Modifier
-        .fillMaxWidth(),
-        colors = CardDefaults.cardColors(
-            containerColor = Blue
-        )
-    ) {
-        Column(modifier = Modifier.padding(15.dp)) {
-            Row {
-                Icon(
-                    imageVector = Icons.Default.DateRange,
-                    contentDescription = "Date",
-                    tint = Color.Black
-                )
-                Spacer()
-                Text(
-                    text = "Reporte diario",
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Black
-                )
-            }
-            Spacer()
-            Row(modifier = Modifier) {
-                Icon(
-                    imageVector = Icons.Default.ShoppingCart,
-                    contentDescription = null, Modifier.size(150.dp),
-                    tint = Color.Black
-                )
-                Column(modifier = Modifier.align(Alignment.CenterVertically)) {
-                    Text(text = "Hay un total de trabajos: ", color = Color.Black)
-                    Text(text = getProyectDetails().size.toString(), color = Color.Black)
-                }
-            }
-        }
-    }
-    Card(modifier = Modifier
-        .fillMaxWidth(),
-        colors = CardDefaults.cardColors(
-            containerColor = Blue
-        )
-    ) {
-        Column(modifier = Modifier.padding(15.dp)) {
-            Row {
-                Icon(
-                    imageVector = Icons.Default.DateRange,
-                    contentDescription = "Date",
-                    tint = Color.Black
-                )
-                Spacer()
-                Text(
-                    text = "Reporte diario",
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Black
-                )
-            }
-            Spacer()
-            Row(modifier = Modifier) {
-                Icon(
-                    imageVector = Icons.Default.ShoppingCart,
-                    contentDescription = null, Modifier.size(150.dp),
-                    tint = Color.Black
-                )
-                Column(modifier = Modifier.align(Alignment.CenterVertically)) {
-                    Text(text = "Hay un total de trabajos: ", color = Color.Black)
-                    Text(text = getProyectDetails().size.toString(), color = Color.Black)
-                }
-            }
-        }
-    }
-}
 }
 
 @Composable
-fun Footer(navigationController: NavHostController) {
+fun FooterDashboard(navigationController: NavHostController) {
         FloatingActionButton(
             onClick = { navigationController.navigate(Screens.CreateProyect.route) },
             containerColor = Blue,
         ) {
             Icon(imageVector = Icons.Default.Add,contentDescription = null, tint = Primary)
         }
+}
+
+@Composable
+fun CardTotal(){
+    Card(modifier = Modifier
+        .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = Blue
+        )
+    ) {
+        Column(modifier = Modifier.padding(15.dp)) {
+            Row {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = "Date",
+                    tint = Color.Black
+                )
+                Spacer()
+                Text(
+                    text = "Reporte diario",
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+            }
+            Spacer()
+            Row(modifier = Modifier) {
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart,
+                    contentDescription = null, Modifier.size(150.dp),
+                    tint = Color.Black
+                )
+                Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+                    Text(text = "Hay un total de trabajos: ", color = Color.Black)
+                    Text(text = getProyectDetails().size.toString(), color = Color.Black)
+                }
+            }
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
